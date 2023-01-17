a77389b7478b67f106a345e7d4092a9d4e58f293
ddoyediran <ddoyediran@gmail.com>
2023-01-17 23:46:18 +0100

    done

--- server.js
+++ server.js
@@ -1,26 +1,64 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const User = require("./model/userModel");
 
 const mongo_url = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 6060;
 
 const app = express();
 
-const users = [
-  { id: 1, name: "user1" },
-  { id: 2, name: "user2" },
-  { id: 3, name: "user3" },
-  { id: 4, name: "user4" },
-  { id: 5, name: "user5" },
-  { id: 6, name: "user6" },
-  { id: 7, name: "user7" },
-  { id: 8, name: "user8" },
-  { id: 9, name: "user9" },
-  { id: 10, name: "user10" },
-];
+// connecting to the database
+mongoose
+  .connect(mongo_url)
+  .then((results) => {
+    console.log("Connected to MongoDB database");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
-app.get("/users", paginate(users), (req, res) => {
+// initializing db to mongoose connection method
+const db = mongoose.connection;
+
+// inserting data to the user collection database
+db.once("open", async () => {
+  if ((await User.countDocuments().exec()) > 0) {
+    return console.log("user already inserted to database");
+  }
+
+  Promise.all([
+    User.create({ name: "user1" }),
+    User.create({ name: "user2" }),
+    User.create({ name: "user3" }),
+    User.create({ name: "user4" }),
+    User.create({ name: "user5" }),
+    User.create({ name: "user6" }),
+    User.create({ name: "user7" }),
+    User.create({ name: "user8" }),
+    User.create({ name: "user9" }),
+    User.create({ name: "user10" }),
+  ])
+    .then(() => console.log("Added users to database"))
+    .catch(() =>
+      console.log("error occurred while inserting data to database")
+    );
+});
+
+// const users = [
+//   { id: 1, name: "user1" },
+//   { id: 2, name: "user2" },
+//   { id: 3, name: "user3" },
+//   { id: 4, name: "user4" },
+//   { id: 5, name: "user5" },
+//   { id: 6, name: "user6" },
+//   { id: 7, name: "user7" },
+//   { id: 8, name: "user8" },
+//   { id: 9, name: "user9" },
+//   { id: 10, name: "user10" },
+// ];
+
+app.get("/users", paginate(User), (req, res) => {
   res.json(res.paginatedResult);
 
   //   const result = {};
@@ -61,7 +99,7 @@ app.listen(PORT, () => {
  */
 
 function paginate(model) {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     const page = parseInt(req.query.page);
     const limit = parseInt(req.query.limit);
     const startIndex = (page - 1) * limit;
@@ -70,7 +108,7 @@ function paginate(model) {
     const result = {};
 
     // to create the next page
-    if (endIndex < model.length) {
+    if (endIndex < (await model.countDocuments().exec())) {
       result.next = {
         page: page + 1,
         limit: limit,
@@ -83,9 +121,12 @@ function paginate(model) {
         limit: limit,
       };
     }
-    // add results to result
-    result.results = model.slice(startIndex, endIndex);
-    res.paginatedResult = result;
-    next();
+    try {
+      result.results = await model.find().limit(limit).skip(startIndex);
+      res.paginatedResult = result;
+      next();
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   };
 }
