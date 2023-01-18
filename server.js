d0de77e0126d655680c1a8ee8999d065e6c1a0a7
ddoyediran <ddoyediran@gmail.com>
2023-01-18 23:40:39 +0100

    clean up

--- server.js
+++ server.js
@@ -47,29 +47,6 @@ db.once("open", async () => {
 
 app.get("/users", paginate(User), (req, res) => {
   res.json(res.paginatedResult);
-
-  //   const result = {};
-  //   const page = parseInt(req.query.page);
-  //   const limit = parseInt(req.query.limit);
-  //   const startIndex = (page - 1) * limit;
-  //   const endIndex = page * limit;
-  //   // to create the next page
-  //   if (endIndex < users.length) {
-  //     result.next = {
-  //       page: page + 1,
-  //       limit: limit,
-  //     };
-  //   }
-  //   // to create previous page
-  //   if (startIndex > 0) {
-  //     result.previous = {
-  //       page: page - 1,
-  //       limit: limit,
-  //     };
-  //   }
-  //   // add results to result
-  //   result.results = users.slice(startIndex, endIndex);
-  //   res.json(result);
 });
 
 // app.get("/posts", paginate(posts), (req, res) => {
@@ -80,11 +57,9 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-/**
- * @input: model
- * @output:
- */
-
+// Helper function perform pagination
+// INPUT: model
+// OUTPUT: returns result of pagination
 function paginate(model) {
   return async (req, res, next) => {
     const page = parseInt(req.query.page);
